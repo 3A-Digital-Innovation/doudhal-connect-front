ef78d26e60b8b7f95f155fca280426bb58fc7909
DIALLO ABOUBACAR <a.diallo3012@gmail.com>
2024-07-07 14:06:06 +0200

    update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/students',
+    // loadChildren: () => import('../app/features/students/student.routes').then(m => m.studentRoutes)
+    loadComponent: () => import('../app/features/students/students.component').then(m => m.StudentsComponent)
+
+  }
+];
