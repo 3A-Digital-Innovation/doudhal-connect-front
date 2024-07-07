f4b0044f641e367d63de36fc9b942bab67c672a1
alpha <alfapellel@gmail.com>
2024-07-07 13:54:41 +0200

    Création de la page de connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    
+    {
+        path: '', 
+        loadChildren: () => import('./features/features.routes').then(mod => mod.FEATURES_ROUTES)
+    },
+];
+
+
---
+++ src/app/features/features.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+
+export const FEATURES_ROUTES: Route[] = [
+    { path: 'login', component: LoginComponent },
+];
